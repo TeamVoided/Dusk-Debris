905a9deb1ed787bfe88a0eb53890b6cb65cc9bc1
theendercore <theendercore@gmail.com>
2024-07-12 21:54:14 +0300

    - root placed

--- src/main/kotlin/org/teamvoided/dusk_debris/world/gen/root/CypressRootPlacer.kt
+++ src/main/kotlin/org/teamvoided/dusk_debris/world/gen/root/CypressRootPlacer.kt
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.block.BlockState
+import net.minecraft.registry.tag.BlockTags
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.int_provider.IntProvider
@@ -16,6 +17,7 @@ import net.minecraft.world.gen.root.RootPlacerType
 import net.minecraft.world.gen.stateprovider.BlockStateProvider
 import org.teamvoided.dusk_debris.init.DuskWorldgen
 import org.teamvoided.dusk_debris.util.Utils.placeDebug
+import org.teamvoided.dusk_debris.util.isInSet
 import org.teamvoided.dusk_debris.util.isInTag
 import org.teamvoided.dusk_debris.world.gen.root.config.CypressRootConfig
 import java.util.*
@@ -26,8 +28,7 @@ class CypressRootPlacer(
     rootProvider: BlockStateProvider,
     aboveRootPlacement: Optional<AboveRootPlacement>,
     private val config: CypressRootConfig
-) :
-    RootPlacer(trunkOffsetY, rootProvider, aboveRootPlacement) {
+) : RootPlacer(trunkOffsetY, rootProvider, aboveRootPlacement) {
     override fun generate(
         world: TestableWorld,
         replacer: BiConsumer<BlockPos, BlockState>,
@@ -41,31 +42,18 @@ class CypressRootPlacer(
 
         replacer.placeDebug(mutable, 6)
 
-        while (mutable.y < trunkPos.y) {
-            if (!this.canReplace(world, mutable)) {
-                return false
-            }
-            mutable.move(Direction.UP)
-            replacer.placeDebug(mutable, 8)
-        }
-
-        list.add(trunkPos.down())
-
         Direction.Type.HORIZONTAL.forEach { direction ->
             val blockPos = trunkPos.offset(direction)
-            val list2: MutableList<BlockPos> = Lists.newArrayList()
-            if (!this.canGrow(world, random, blockPos, direction, trunkPos, list2, 0)) {
-                return false
+            if (canReplace(world, blockPos)) {
+                list.add(trunkPos.offset(direction))
             }
-
-            list.addAll(list2)
-            list.add(trunkPos.offset(direction))
         }
 
         list.forEach { posB ->
             replacer.placeDebug(posB, 9)
-//            this.placeRoot(world, replacer, random, posB, config)
+            this.placeRoot(world, replacer, random, posB.down(), config) // .down() is temporary
         }
+
 
         return true
     }
@@ -79,41 +67,32 @@ class CypressRootPlacer(
         potentialRootPositions: MutableList<BlockPos>,
         rootLength: Int
     ): Boolean {
-        val i = config.maxRootLength
-        if (rootLength != i && potentialRootPositions.size <= i) {
-            val list = this.getPotentialRootPositions(pos, direction, random, origin)
-            val var10: Iterator<*> = list.iterator()
-
-            while (var10.hasNext()) {
-                val blockPos = var10.next() as BlockPos
-                if (this.canReplace(world, blockPos)) {
-                    potentialRootPositions.add(blockPos)
-                    if (!this.canGrow(
-                            world,
-                            random,
-                            blockPos,
-                            direction,
-                            origin,
-                            potentialRootPositions,
-                            rootLength + 1
-                        )
-                    ) {
-                        return false
-                    }
-                }
-            }
-
-            return true
-        } else {
+//        val i = config.maxRootLength
+//        if (rootLength != i && potentialRootPositions.size <= i) {
+//            val list = this.getPotentialRootPositions(pos, direction, random, origin)
+//            val var10: Iterator<*> = list.iterator()
+//
+//            while (var10.hasNext()) {
+//                val blockPos = var10.next() as BlockPos
+//                if (this.canReplace(world, blockPos)) {
+//                    potentialRootPositions.add(blockPos)
+//                    if (!this.canGrow(
+//                            world, random, blockPos, direction, origin, potentialRootPositions, rootLength + 1
+//                        )
+//                    ) {
+//                        return false
+//                    }
+//                }
+//            }
+//
+//            return true
+//        } else {
             return false
-        }
+//        }
     }
 
     protected fun getPotentialRootPositions(
-        pos: BlockPos,
-        direction: Direction?,
-        random: RandomGenerator,
-        origin: BlockPos?
+        pos: BlockPos, direction: Direction?, random: RandomGenerator, origin: BlockPos?
     ): List<BlockPos> {
         val blockPos = pos.down()
         val blockPos2 = pos.offset(direction)
@@ -132,7 +111,9 @@ class CypressRootPlacer(
     }
 
     override fun canReplace(world: TestableWorld, pos: BlockPos): Boolean {
-        return super.canReplace(world, pos) || world.isInTag(pos, config.canGrowThrough)
+        return super.canReplace(world, pos)
+                || world.isInSet(pos, config.canGrowThrough)
+                || world.isInTag(pos, BlockTags.REPLACEABLE)
     }
 
     override fun placeRoot(
@@ -142,10 +123,11 @@ class CypressRootPlacer(
         pos: BlockPos,
         config: TreeFeatureConfig
     ) {
-        if (world.isInTag(pos, this.config.muddyRootsIn)) {
-            val blockState = this.config.muddyRootsProvider.getBlockState(random, pos)
-            replacer.accept(pos, this.applyWaterlogging(world, pos, blockState))
-        } else super.placeRoot(world, replacer, random, pos, config)
+        var mPos = pos
+        while (canReplace(world, mPos)) {
+            replacer.placeDebug(mPos, 5)
+            mPos = mPos.down()
+        }
     }
 
     override fun getType(): RootPlacerType<CypressRootPlacer> = DuskWorldgen.CYPRESS_ROOT_PLACER
@@ -155,8 +137,7 @@ class CypressRootPlacer(
         const val MAX_ROOT_LENGTH: Int = 15
         val CODEC: MapCodec<CypressRootPlacer> = RecordCodecBuilder.mapCodec {
             rootPlacerCodec(it).and(
-                CypressRootConfig.CODEC.fieldOf("cypress_root_placement")
-                    .forGetter(CypressRootPlacer::config)
+                CypressRootConfig.CODEC.fieldOf("cypress_root_placement").forGetter(CypressRootPlacer::config)
             ).apply(it, ::CypressRootPlacer)
         }
     }
