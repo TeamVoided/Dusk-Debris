73dca829df8d8c037ded20d4b1678f3bfad3b754
TheDwindlingLight <genericperson1080@gmail.com>
2025-05-08 22:33:11 -0400

    abstracting cube particles

---
+++ src/client/kotlin/org/teamvoided/dusk_debris/particle/stupid_particles/abstracts/AbstractCubeParticle.kt
@@ -0,0 +1,75 @@
+package org.teamvoided.dusk_debris.particle.stupid_particles.abstracts
+
+import com.mojang.blaze3d.vertex.VertexConsumer
+import net.minecraft.client.particle.BillboardParticle
+import net.minecraft.client.particle.ParticleTextureSheet
+import net.minecraft.client.particle.SpriteProvider
+import net.minecraft.client.world.ClientWorld
+import net.minecraft.util.math.MathHelper
+import net.minecraft.util.math.Vec3d
+import org.joml.Quaternionf
+import org.joml.Vector3f
+import org.joml.Vector4f
+import org.teamvoided.dusk_debris.particle.stupid_particles.models.CubeSimple
+import org.teamvoided.dusk_debris.particle.stupid_particles.models.CubeUnwrapped
+
+abstract class AbstractCubeParticle(
+    world: ClientWorld,
+    x: Double,
+    y: Double,
+    z: Double,
+    xVel: Double,
+    yVel: Double,
+    zVel: Double
+) : SpriteBillboardKotlinParticle(world, x, y, z, xVel, yVel, zVel) {
+    private var cubeShape: CubeSimple? = null
+    private var prevRot: Vec3d = Vec3d.ZERO
+    private var rotation: Vec3d = Vec3d.ZERO
+    private var rotSpeed: Vec3d = Vec3d.ZERO
+
+
+    override fun drawParticle(
+        vertexConsumer: VertexConsumer,
+        quaternionf: Quaternionf,
+        x: Float,
+        y: Float,
+        z: Float,
+        tickDelta: Float
+    ) {
+        if (cubeShape != null) {
+            val color = Vector4f(this.colorRed, this.colorGreen, this.colorBlue, this.colorAlpha)
+            val brightness = this.getBrightness(tickDelta)
+            val size = this.getSize(tickDelta)
+            cubeShape!!.renderCube(vertexConsumer, quaternionf, x, y, z, color, brightness, size)
+        } else {
+            println("set sprite, fool")
+            this.markDead()
+        }
+    }
+
+    open fun getRotation(tickDelta: Float): Vector3f {
+        val x = MathHelper.lerp(tickDelta.toDouble(), prevRot.x, rotation.x).toFloat()
+        val y = MathHelper.lerp(tickDelta.toDouble(), prevRot.y, rotation.y).toFloat()
+        val z = MathHelper.lerp(tickDelta.toDouble(), prevRot.z, rotation.z).toFloat()
+        return Vector3f(x, y, z)
+    }
+
+    override fun setSprite(spriteProvider: SpriteProvider) {
+        super.setSprite(spriteProvider)
+        createShape()
+        //if the particle texture changes size, simply copy this for setSpriteForAge
+    }
+
+    open fun createShape() {
+        this.cubeShape = CubeUnwrapped(this.minU(), this.maxU(), this.minV(), this.maxV())
+    }
+
+    override val particleRotation = BillboardParticle.FacingCameraMode { rotation: Quaternionf, _, tickDelta: Float ->
+        val rotate = this.getRotation(tickDelta)
+        rotation.rotateZ(rotate.x)
+        rotation.rotateY(rotate.y)
+        rotation.rotateX(rotate.z)
+    }
+
+    override fun getType(): ParticleTextureSheet = ParticleTextureSheet.PARTICLE_SHEET_OPAQUE
+}
---
+++ src/main/kotlin/org/teamvoided/dusk_debris/world/gen/trunk/BlueTrunkPlacer.kt
@@ -0,0 +1,89 @@
+package org.teamvoided.dusk_debris.world.gen.trunk
+
+import com.google.common.collect.Lists
+import com.mojang.serialization.MapCodec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.block.BlockState
+import net.minecraft.util.dynamic.Codecs
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.int_provider.IntProvider
+import net.minecraft.util.random.RandomGenerator
+import net.minecraft.world.TestableWorld
+import net.minecraft.world.gen.feature.TreeFeature
+import net.minecraft.world.gen.feature.TreeFeatureConfig
+import net.minecraft.world.gen.foliage.FoliagePlacer
+import net.minecraft.world.gen.trunk.TrunkPlacer
+import net.minecraft.world.gen.trunk.TrunkPlacerType
+import java.util.function.BiConsumer
+
+class BlueTrunkPlacer(
+    baseHeight: Int,
+    firstRandomHeight: Int,
+    secondRandomHeight: Int,
+    private val minHeightForLeaves: Int,
+    private val bendLength: IntProvider
+) : TrunkPlacer(baseHeight, firstRandomHeight, secondRandomHeight) {
+    override fun getType(): TrunkPlacerType<*> {
+        return TrunkPlacerType.BENDING_TRUNK_PLACER
+    }
+
+    override fun generate(
+        world: TestableWorld,
+        replacer: BiConsumer<BlockPos, BlockState>,
+        random: RandomGenerator,
+        height: Int,
+        startPos: BlockPos,
+        config: TreeFeatureConfig
+    ): List<FoliagePlacer.TreeNode> {
+        val direction = Direction.Type.HORIZONTAL.random(random)
+        val treeHeight = height - 1
+        val mutable = startPos.mutableCopy()
+        val downPos = mutable.down()
+        setToDirt(world, replacer, random, downPos, config)
+        val list: MutableList<FoliagePlacer.TreeNode> = Lists.newArrayList()
+        var loop = 0
+        while (loop <= treeHeight) {
+            if (loop + 1 >= treeHeight + random.nextInt(2)) {
+                mutable.move(direction)
+            }
+
+            if (TreeFeature.canReplace(world, mutable)) {
+                this.placeTrunkBlock(world, replacer, random, mutable, config)
+            }
+
+            if (loop >= this.minHeightForLeaves) {
+                list.add(FoliagePlacer.TreeNode(mutable.toImmutable(), 0, false))
+            }
+
+            mutable.move(Direction.UP)
+            ++loop
+        }
+
+        loop = bendLength[random]
+
+        for (k in 0..loop) {
+            if (TreeFeature.canReplace(world, mutable)) {
+                this.placeTrunkBlock(world, replacer, random, mutable, config)
+            }
+
+            list.add(FoliagePlacer.TreeNode(mutable.toImmutable(), 0, false))
+            mutable.move(direction)
+        }
+
+        return list
+    }
+
+    companion object {
+        val CODEC: MapCodec<BlueTrunkPlacer> =
+            RecordCodecBuilder.mapCodec { instance: RecordCodecBuilder.Instance<BlueTrunkPlacer> ->
+                fillTrunkPlacerFields(instance).and(
+                    instance.group(
+                        Codecs.POSITIVE_INT.optionalFieldOf("min_height_for_leaves", 1)
+                            .forGetter { placer: BlueTrunkPlacer -> placer.minHeightForLeaves },
+                        IntProvider.method_35004(1, 64).fieldOf("bend_length")
+                            .forGetter { placer: BlueTrunkPlacer -> placer.bendLength })
+                ).apply(instance, ::BlueTrunkPlacer)
+            }
+    }
+}
